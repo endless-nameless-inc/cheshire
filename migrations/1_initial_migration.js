5f4e9af1a6f51375c0fa15080132b539f451e393
Eugene Otto <eugene.otto@gmail.com>
2018-08-01 11:56:31 -0700

    Restore Truffle's Migrations contract

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,4 @@
+const Migrations = artifacts.require("./Migrations.sol")
 const GeneScience = artifacts.require("./GeneScience.sol")
 const KittyCore = artifacts.require("./KittyCore.sol")
 const SaleClockAuction = artifacts.require("./SaleClockAuction.sol")
@@ -5,19 +6,22 @@ const SiringClockAuction = artifacts.require("./SiringClockAuction.sol")
 
 module.exports = (deployer, network) => {
   deployer.then(async () => {
+    const migrations = await deployer.deploy(Migrations)
+    console.log('Migrations contract deployed at', migrations.address)
+
     const kittyCore = await deployer.deploy(KittyCore)
-    console.log('kittyCore deployed at', kittyCore.address)
+    console.log('KittyCore contract deployed at', kittyCore.address)
 
     const ownerCut = 375
 
     const saleClockAuction = await deployer.deploy(SaleClockAuction, kittyCore.address, ownerCut)
-    console.log('SaleClockAuction deployed at', saleClockAuction.address)
+    console.log('SaleClockAuction contract deployed at', saleClockAuction.address)
 
     const siringClockAuction = await deployer.deploy(SiringClockAuction, kittyCore.address, ownerCut)
-    console.log('SiringClockAuction deployed at', siringClockAuction.address)
+    console.log('SiringClockAuction contract deployed at', siringClockAuction.address)
 
     const geneScience = await deployer.deploy(GeneScience)
-    console.log('geneScience deployed at', geneScience.address)
+    console.log('GeneScience contract deployed at', geneScience.address)
 
     await kittyCore.setSaleAuctionAddress(saleClockAuction.address)
     await kittyCore.setSiringAuctionAddress(siringClockAuction.address)
